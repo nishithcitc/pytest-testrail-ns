62b1070a341d6e3da3dcb6516a84fab025c7000c
Nishith Shah <nshthshah@gmail.com>
2021-10-05 10:58:53 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -7,21 +7,21 @@ def read_file(fname):
 
 
 setup(
-    name='pytest-testrail-e2e',
+    name='pytest-testrail-ns',
     description='pytest plugin for creating TestRail runs and adding results',
     long_description=read_file('README.rst'),
-    version='3.0.1',
+    version='1.0.0',
     author='NGUYEN Viet',
     author_email='vietnq254@live.com',
-    url='https://github.com/vietnq254/pytest-testrail-e2e/',
+    url='https://github.com/nishithcitc/pytest-testrail-ns',
     packages=[
         'pytest_testrail',
     ],
-    package_dir={'pytest_testrail_e2e': 'pytest_testrail_e2e'},
+    package_dir={'pytest_testrail_ns': 'pytest_testrail_ns'},
     install_requires=[
         'pytest>=3.6',
         'requests>=2.20.0',
     ],
     include_package_data=True,
-    entry_points={'pytest11': ['pytest-testrail-e2e = pytest_testrail.conftest']},
+    entry_points={'pytest11': ['pytest-testrail-ns = pytest_testrail.conftest']},
 )
